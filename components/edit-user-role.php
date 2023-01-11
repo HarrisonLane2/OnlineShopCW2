79b598de35e850723dea4e67e16c9d633212cce2
Harrison Lane <43409781+HarrisonLane2@users.noreply.github.com>
2023-01-11 10:20:19 +0000

    Added edit user roles form

--- components/edit-user-role.php
+++ components/edit-user-role.php
@@ -37,3 +37,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 } 
 
 ?>
+
+<form method="post" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" enctype="multipart/form-data">
+    <section class="vh-100">
+      <div class="container py-5 h-75">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card shadow-2-strong" style="border-radius: 1rem;">
+              <div class="card-body p-5 text-center">
+    
+                <h3 class="mb-2">Add user Roles</h3>
+                <div class="form-outline mb-4">
+                  <input type="text" id="userID" name="UserID" class="form-control form-control-lg" placeholder="UserID " required value="<?= htmlspecialchars($userID['value'] ?? '') ?>"/>
+                  <span class="text-danger"><?= $userID['error'] ?? '' ?></span>
+                </div>
+                
+                <div class="form-outline mb-4">
+                  <input type="text" id="admin" name="admin" class="form-control form-control-lg" placeholder="Please enter a 1 for yes and a 0 for no" required value="<?= htmlspecialchars($admin['value'] ?? '') ?>"/>
+                  <span class="text-danger"><?= $admin['error'] ?? '' ?></span>
+                </div>
+    
+    
+                <div class="form-outline mb-4">
+                  <input type="number" id="staff" name="staff" class="form-control form-control-lg" placeholder="Please enter a 1 for yes and a 0 for no" required value="<?= htmlspecialchars($staff['value'] ?? '') ?>"/>
+                  <span class="text-danger"><?= $staff['error'] ?? '' ?></span>
+                </div>
+    
+                <button class="btn btn-primary btn-lg w-100 mb-4" type="submit">Add Product</button>
+               
+                <?= isset($_GET['errmsg']) ? $message = $_GET['errmsg'] : '' ?>
+                <?= $message ? alert($message, 'danger') : '' ?>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </form>
